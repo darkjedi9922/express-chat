7238a32d1a9bb991da66fb3cf748534d8a03e553
Jed Dark <darkjedi9922@gmail.com>
2020-07-05 08:17:15 +0300

    Move dialog list page into dialogs index route and add login page.

--- routes/dialogs.js
+++ routes/dialogs.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/', (req, res, next) => {
+    res.render('dialogs/index')
+});
+
 router.get('/add', function(req, res, next) {
     res.render('dialogs/add')
 });
