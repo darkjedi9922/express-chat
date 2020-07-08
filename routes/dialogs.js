bffb9db0f470fbc914a1157b4e052cfe34677eab
Jed Dark <darkjedi9922@gmail.com>
2020-07-08 08:58:51 +0300

    Make some DRY improvements.

--- routes/dialogs.js
+++ routes/dialogs.js
@@ -26,68 +26,77 @@ router.get('/add', async (req, res) => {
     })
 });
 
-router.post('/add', validateWithFlashBack([
-    body('title', 'Название не указано').exists().notEmpty()
-]), async (req, res) => {
-    const newDialog = await (await db()).dialogs.insert({
-        id: shortid.generate(),
-        title: req.body.title,
-        members: [req.cookies.token],
-        messages: []
-    });
-    res.redirect(`/dialogs/${newDialog.id}`);
-});
+router.post('/add', 
+    validateWithFlashBack([
+        body('title', 'Название не указано').exists().notEmpty()
+    ]),
+    async (req, res) => {
+        const newDialog = await (await db()).dialogs.insert({
+            id: shortid.generate(),
+            title: req.body.title,
+            members: [req.cookies.token],
+            messages: []
+        });
+        res.redirect(`/dialogs/${newDialog.id}`);
+    }
+);
 
-router.get('/:id', async (req, res, next) => {
-    const appDb = await db();
-    const dialog = await appDb.dialogs.findOne(req.params.id).exec();
-    if (!dialog) return next(createHttpError(404));
-
-    res.locals.appDb = appDb;
-    res.locals.dialog = dialog;
-    next();
-}, requireAccess(canPostMessage), async (req, res) => {
-    const appDb = res.locals.appDb;
-    const dialog = res.locals.dialog;
-    res.render('dialogs/item', {
-        token: req.cookies.token,
-        dialogs: await appDb.dialogs.findByMember(req.cookies.token),
-        currentDialog: dialog,
-        messages: await Promise.all((await dialog.populate('messages') || []).map(async (message) => ({
-            item: message,
-            author: await appDb.users.findOne(message.authorId).exec()
-        }))),
-        errors: req.errors
-    })
-});
-
-router.post('/:id', async (req, res, next) => {
-    const appDb = await db();
-    const dialog = await appDb.dialogs.findOne(req.params.id).exec();
-    if (!dialog) return next(createHttpError(404));
-    
-    res.locals.appDb = appDb;
-    res.locals.dialog = dialog;
-    next();
-}, requireAccess(canPostMessage), validateWithFlashBack([
-    body('text', 'Текст сообщения не указан').exists().notEmpty()
-]), async (req, res) => {
-    const newMessageId = shortid.generate();
-    await Promise.all([
-        res.locals.appDb.messages.insert({
-            id: newMessageId,
-            dialogId: req.params.id,
-            message: req.body.text,
-            authorToken: req.cookies.token,
-            createdAt: Date.now()
-        }),
-        res.locals.dialog.atomicUpdate(oldData => {
-            oldData.messages.push(newMessageId);
-            return oldData;
+router.get('/:id',
+    _requireDialogExistsById(),
+    requireAccess(canPostMessage),
+    async (req, res) => {
+        const appDb = res.locals.appDb;
+        const dialog = res.locals.dialog;
+        const messages = await dialog.populate('messages') || [];
+        res.render('dialogs/item', {
+            token: req.cookies.token,
+            dialogs: await appDb.dialogs.findByMember(req.cookies.token),
+            currentDialog: dialog,
+            messages: await Promise.all(messages.map(async (message) => ({
+                item: message,
+                author: await appDb.users.findOne(message.authorId).exec()
+            }))),
+            errors: req.errors
         })
-    ]);
+    }
+);
 
-    res.redirect('back');
-})
+router.post('/:id',
+    _requireDialogExistsById(),
+    requireAccess(canPostMessage),
+    validateWithFlashBack([
+        body('text', 'Текст сообщения не указан').exists().notEmpty()
+    ]),
+    async (req, res) => {
+        const newMessageId = shortid.generate();
+        await Promise.all([
+            res.locals.appDb.messages.insert({
+                id: newMessageId,
+                dialogId: req.params.id,
+                message: req.body.text,
+                authorToken: req.cookies.token,
+                createdAt: Date.now()
+            }),
+            res.locals.dialog.atomicUpdate(oldData => {
+                oldData.messages.push(newMessageId);
+                return oldData;
+            })
+        ]);
+
+        res.redirect('back');
+    }
+)
+
+function _requireDialogExistsById() {
+    return async (req, res, next) => {
+        const appDb = await db();
+        const dialog = await appDb.dialogs.findOne(req.params.id).exec();
+        if (dialog) {
+            res.locals.appDb = appDb;
+            res.locals.dialog = dialog;
+            next();
+        } else next(createHttpError(404));
+    }
+} 
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -7,23 +7,29 @@ const { requireAccess } = require('../rights/helpers');
 const { isGuest } = require('../rights/users');
 const db = require('../db/connect');
 
-router.get('/', requireAccess(isGuest, '/dialogs'), async (req, res) => {
-    res.render('index', { title: 'Express', errors: req.errors });
-});
+router.get('/',
+    requireAccess(isGuest, '/dialogs'),
+    async (req, res) => {
+        res.render('index', { title: 'Express', errors: req.errors });
+    }
+);
 
-router.post('/login', validateWithFlashBack([
-    body('login', 'Логин не передан').exists().notEmpty(),
-    body('password', 'Пароль не передан').exists().notEmpty(),
-]), async (req, res) => {
-    const userToken = hash(`${req.body.login}${req.body.password}`);
-    const appDb = await db();
-    if (! await appDb.users.findByToken(userToken)) await appDb.users.insert({
-        token: userToken,
-        login: req.body.login
-    });
+router.post('/login', 
+    validateWithFlashBack([
+        body('login', 'Логин не передан').exists().notEmpty(),
+        body('password', 'Пароль не передан').exists().notEmpty(),
+    ]),
+    async (req, res) => {
+        const userToken = hash(`${req.body.login}${req.body.password}`);
+        const appDb = await db();
+        if (! await appDb.users.findByToken(userToken)) await appDb.users.insert({
+            token: userToken,
+            login: req.body.login
+        });
 
-    res.cookie('token', userToken).redirect('/dialogs');
-});
+        res.cookie('token', userToken).redirect('/dialogs');
+    }
+);
 
 router.get('/logout', (req, res) => {
     res.clearCookie('token').redirect('/');
